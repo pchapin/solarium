4537426e1f8a2142345b21a7219449734afa18ac
Peter Chapin <chapinp@acm.org>
2020-04-22 16:55:30 -0400

    Added a skeletal project for the Scala version of Solarium.

---
+++ Scala/build.sbt
@@ -0,0 +1,8 @@
+name := "Solarium"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
+
+libraryDependencies +=
+  "org.scala-lang.modules" %% "scala-parallel-collections" % "0.2.0"
---
+++ Scala/src/main/scala/org/pchapin/solarium/Main.scala
@@ -0,0 +1,20 @@
+package org.pchapin.solarium
+
+import scala.collection.immutable.Vector
+import scala.collection.parallel.CollectionConverters._
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello, World")
+
+    val myVector = Vector(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val resultVector1 = myVector map { _ + 1 }
+    println(resultVector1)
+
+    val myParallelVector = myVector.par
+    val resultVector2 = myParallelVector map { _ + 1 }
+    println(resultVector2)
+  }
+
+}
